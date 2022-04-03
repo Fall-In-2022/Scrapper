22b0a48bfb76680008561f58e7d73d075e318e5d
Ricardo Di Zio <ricardodizio@gmail.com>
2022-04-03 03:16:41 -0700

    Reddit config

--- config/reddit-api.php
+++ config/reddit-api.php
@@ -4,10 +4,10 @@ return [
     'endpoint_standard' => 'https://www.reddit.com',
     'endpoint_oauth' => 'https://oauth.reddit.com',
 
-    'username' => 'ri_dizio',
-    'password' => 'FUb61d4fddpd',
-    'app_id' => 'PI71iiLbseWNmRfPZTOvcA',
-    'app_secret' => 'g3ba0ucDhI3afeP-oa_op_5mHsOBqw',
+    'username' => '',
+    'password' => '',
+    'app_id' => '',
+    'app_secret' => '',
 
     'response_format' => 'STD', // STD | ARRAY
 
