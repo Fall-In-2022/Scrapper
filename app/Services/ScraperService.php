a612b0fc6ec16a4ed15ac0365e2334ba4e8866b2
Ricardo Di Zio <ricardodizio@gmail.com>
2022-04-03 10:50:24 -0700

    Cache System

--- app/Services/ScraperService.php
+++ app/Services/ScraperService.php
@@ -8,6 +8,7 @@ use App\Models\UkraineCity;
 use Illuminate\Support\Str;
 use App\Helpers\LocationHelper;
 use Atymic\Twitter\Facade\Twitter;
+use Illuminate\Support\Facades\Cache;
 
 class ScraperService
 {
@@ -184,9 +185,22 @@ class ScraperService
 
         foreach($cities as $city) {
 
-            $responseTwitter = $twitter->getSearch([
-                "q"=> $city->city_name
-            ]);
+            $cityCacheKey = $city->city_name;
+
+            if( Cache::store('redis')->get("tweets:$cityCacheKey") != NULL ) {
+
+                $responseTwitter = json_decode(Cache::get("tweets:$cityCacheKey"));
+
+
+            } else {
+
+                $responseTwitter = $twitter->getSearch([
+                    "q"=> $city->city_name
+                ]);
+
+                Cache::store('redis')->put("tweets:$cityCacheKey", json_encode($responseTwitter), 900);
+
+            }
 
             $data = [
                 "city_info" => $city,
