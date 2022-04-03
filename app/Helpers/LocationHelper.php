58655df054e9ca8f9b661203fb965b441fec9c4d
Ricardo Di Zio <ricardodizio@gmail.com>
2022-04-03 01:08:12 -0700

    Location Helper

---
+++ app/Helpers/LocationHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use Illuminate\Support\Facades\DB;
+
+
+/**
+ * Location Helper Class
+ */
+class LocationHelper
+{
+    /**
+     * Get Nearest City function
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @return array with cites near by given a latitude and longitude with miles distance
+     */
+    public static function getNearestCity(float $latitude, float $longitude) {
+
+        $sql = "SELECT *,
+        (DEGREES(ACOS(SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)) +
+        COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS((($longitude) - (longitude)))))) * 69 ) as MI FROM ukraine_cities
+        WHERE  (DEGREES(ACOS(SIN(RADIANS($latitude)) * SIN(RADIANS(latitude)) +
+        COS(RADIANS($latitude)) * COS(RADIANS(latitude)) * COS(RADIANS((($longitude) - (longitude)))))) * 69 ) <= 20 ORDER BY MI ASC";
+
+        return DB::select( DB::raw($sql));
+
+    }
+}
